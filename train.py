f01fbc751b331b5dceb00a334a3eba9fff91142f
eanemo <eaguilera@nemotec.org>
2021-03-18 10:58:34 +0100

    Logging

--- train.py
+++ train.py
@@ -147,5 +147,6 @@ if __name__ == '__main__':
     training_dataset = BrainS18Dataset(sets.data_root, sets.img_list, sets)
     data_loader = DataLoader(training_dataset, batch_size=sets.batch_size, shuffle=True, num_workers=sets.num_workers, pin_memory=sets.pin_memory)
 
+    print("Using params to train:", sets)
     # training
     train(data_loader, model, optimizer, scheduler, total_epochs=sets.n_epochs, save_interval=sets.save_intervals, save_folder=sets.save_folder, sets=sets) 
